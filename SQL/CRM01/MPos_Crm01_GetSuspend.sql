216d151d3065d9fb686c45863215b5553e596ebb
wdpmaster <249230266@qq.com>
2026-01-09 18:04:06 +0800

    更新脚本，获取挂起列表增加挂起号的返回

--- SQL/CRM01/MPos_Crm01_GetSuspend.sql
+++ SQL/CRM01/MPos_Crm01_GetSuspend.sql
@@ -3,55 +3,51 @@ DROP PROC MPos_crm01_GetSuspend
 GO
 
 CREATE PROC MPos_crm01_GetSuspend
-  @TransDate smalldatetime,
-  @Shop      char(5),
-  @Crid      char(3)
+   @TransDate smalldatetime,
+   @Shop      char(5),
+   @Crid      char(3)
 AS
-    SET NOCOUNT ON;
-    
-    CREATE TABLE #t
-    (
-       tcardID  varchar( 36 ),
-       ttime    smalldatetime,
-       tqty     int,
-       tamt     money,
-       twwscard char( 10 ) DEFAULT '',
-       tsalm    varchar( 40 ) DEFAULT '',
-       PRIMARY KEY CLUSTERED(tcardID)
-    )
+   SET NOCOUNT ON
 
-    INSERT #t
-           (tcardID,ttime,tqty,tamt)
-    SELECT crcart.cartid,max(InputTime),sum(CASE
+   CREATE TABLE #t
+   (
+      tcardID     varchar( 36 ),
+      ttime       smalldatetime,
+      tqty        int,
+      tamt        money,
+      twwscard    char( 10 ) DEFAULT '',
+      tsalm       varchar( 40 ) DEFAULT '',
+      tsuspendnum varchar( 50 ) DEFAULT '',
+      PRIMARY KEY CLUSTERED(tcardID)
+   )
+
+   INSERT #t
+          (tcardID,ttime,tqty,tamt)
+   SELECT crcart.cartid,max(InputTime),sum(CASE
                                               WHEN ItemType = 'S' THEN qty
                                               ELSE 0 - qty
-                                            END),sum(CASE
+                                           END),sum(CASE
                                                        WHEN ItemType = 'S' THEN Amnt
                                                        ELSE 0 - Amnt
-                                                     END)
-    FROM   crcart(nolock),crcarh(nolock)
-    WHERE  crcart.transdate = @TransDate AND
-           crcart.shop = @Shop AND
-           crcart.crid = @Crid AND
-           crcart.TransDate = crcarh.TransDate AND
-           crcart.shop = crcarh.Shop AND
-           crcart.Crid = crcarh.Crid AND
-           crcart.CartID = crcarh.CartID
-    GROUP  BY crcart.CartID
+                                                    END)
+   FROM   crcart(nolock),crcarh(nolock)
+   WHERE  crcart.transdate = @TransDate AND
+          crcart.shop = @Shop AND
+          crcart.crid = @Crid AND
+          crcart.TransDate = crcarh.TransDate AND
+          crcart.shop = crcarh.Shop AND
+          crcart.Crid = crcarh.Crid AND
+          crcart.CartID = crcarh.CartID
+   GROUP  BY crcart.CartID
 
-    UPDATE #t
-    SET    twwscard = wwscard,
-           tsalm = salm
-    FROM   crcarh
-    WHERE  cartid = tcardID
+   UPDATE #t
+   SET    twwscard = wwscard,
+          tsalm = salm,
+          tsuspendnum = SuspendNum
+   FROM   crcarh
+   WHERE  cartid = tcardID
 
-    SELECT a.tcardID CartID,
-           a.ttime [SuspendTime],
-           a.tqty [Qty],
-           a.tamt [Amount],
-           a.twwscard [MemberCard],
-           a.tsalm [SalesAssociate]
-    FROM   #t a
-    ORDER  BY ttime DESC
-
-go 
+   SELECT a.tcardID CartID,a.ttime [SuspendTime],a.tqty [Qty],a.tamt [Amount],a.twwscard [MemberCard],a.tsalm [SalesAssociate],tsuspendnum SuspendNum
+   FROM   #t a
+   ORDER  BY ttime DESC 
+ 
